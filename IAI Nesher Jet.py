1f9775aa02f4c1f7f8454ba89a902b403b9b811b
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 01:19:12 +0500

    Create IAI Nesher Jet.py

---
+++ IAI Nesher Jet.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'IAI Nesher Jet').hexdigest()
+'02a1d595637eea4f289b459c339643f5a1ba218f9dc3557c2ed5af06331de4d4f2ee270e5e97fc630d6b9a615d7351567654dd78e430ba5b5357b5ec76dbe220'
